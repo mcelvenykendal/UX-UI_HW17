0059ad7b2a375a0c24533841759fa3b5b74f6309
mcelvenykendal <63926116+mcelvenykendal@users.noreply.github.com>
2020-09-21 19:05:21 -0700

    transitions

---
+++ Class18.3/index.js
@@ -0,0 +1,4 @@
+var stringTwo= '2';
+var numberTwo= 2;
+
+console.log( 2, 2)
---
+++ Class_19.2/js/index.js
@@ -0,0 +1,8 @@
+// Create event listener to enter items below
+$("#enter").on("click", function(){
+    var task = $("#todoItem").val();
+    $("#todoList").append("<div class='task'>" + task + "<div class='x fas fa-times'></div></div>");
+    
+// Create event listener to delete items below
+
+// Create event listener to toggle 
